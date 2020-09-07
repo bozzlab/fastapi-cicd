774d2d3071a871ffd60d6d0ccb79239de1f59a21
Bozz <p.srinikorn@gmail>
2020-09-07 15:28:15 +0700

    update example for gist [done p1]

--- code_for_gist.py
+++ code_for_gist.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI, Header, Body
+from fastapi import FastAPI, Header, Cookie, Body, File, UploadFile
 from app.db import TSUTAYA_MOVIES, TSUTAYA_MEMBER
+from starlette.responses import JSONResponse
+from app.helper import generate_id
 from pydantic import BaseModel
+from enum import Enum
 import uvicorn 
 
+class Genre(str, Enum):
+    action = "action"
+    sci_fi = "sci-fi"
+    romantic = "romantic"
+    horror = "horror"
+    drama = "drama"
+    adult = "adult"
+
 class MoviesObject(BaseModel):
-    name : str 
+    name : str = "Untitled"
     genre : Genre
 
 app = FastAPI()
@@ -18,14 +29,30 @@ async def fetch_movies(name : str = None):
 
 @app.get("/member")
 async def check_member(x_username : str = Header(...)):
-    if [TSUTAYA_MEMBER[member] for member in TSUTAYA_MEMBER if TSUTAYA_MEMBER[member]['name'] == x_username]:
+    if [ member for member in TSUTAYA_MEMBER if TSUTAYA_MEMBER[member]['name'] == x_username]:
         return {"message" : f"{x_username} is member of TSUTAYA store"}
     else: 
         return {"message" : "Not found"}
 
-app.post("/movies")
-async def insert_movies(req_body : MoviesObject = Body(...)):
-    pass
+@app.get("/member/token")
+async def check_token(x_token : str = Cookie(None)):
+    if x_token in TSUTAYA_MEMBER.keys():
+        return {"message" : f"{TSUTAYA_MEMBER[x_token]['name']} is member of TSUTAYA store and Token is valid"}
+    else: 
+        return {"message" : "Invalid token"}
+
+@app.post("/movies")
+async def insert_movies(req_body : MoviesObject = Body(...), x_token : str = Cookie(None)):
+    movie_id = generate_id()
+    TSUTAYA_MOVIES.update({ movie_id : {"name" : req_body.name,
+                                        "genre" : req_body.genre ,
+                                        "created_by" : TSUTAYA_MEMBER[x_token]['name']}})
+    return JSONResponse(content = {"message" : f"{req_body.name}, added to TSUTAYA store"}, status_code = 201)
+
+@app.post("/member/profile")
+async def insert_image_profile(data : UploadFile = File(...)):
+    image = await data.read()
+    return {"message" : f"{data.filename}, size {len(image)} bytes, updated to your profile"}
 
 if __name__ == "__main__":
     uvicorn.run("code_for_gist:app", host = "0.0.0.0", port = 8080, debug = True)
